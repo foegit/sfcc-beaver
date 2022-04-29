fbc16fc8518c4400f2b746a54a066efb9522b95c
Serhii H <foegit@gmail.com>
2022-04-29 10:59:22 +0300

    Cartridge weight draft

--- src/classes/override/implementation/FileOverrider.ts
+++ src/classes/override/implementation/FileOverrider.ts
@@ -34,8 +34,14 @@ class FileOverrider {
     }
 
     private async selectTargetCartridge() {
-        const cartridgesList = this.sfccProject.getCartridges().map(cartridge => cartridge.getName());
-        const selectedCartridgeName = await vscode.window.showQuickPick(cartridgesList) || '';
+        const cartridgesList1 = this.sfccProject.getCartridges().map(cartridge => cartridge.getName());
+        const cartridgesList2 = this.sortCartridges(cartridgesList1);
+        const cl3 : string[] = cartridgesList2.map(c => {
+            // @ts-ignore
+            return c.name + '(' + c.weight + ')';
+        });
+
+        const selectedCartridgeName = await vscode.window.showQuickPick(cl3) || '';
         const selectedCartridge = this.sfccProject.getCartridgeByName(selectedCartridgeName);
 
         if (!selectedCartridge) {
@@ -43,6 +49,45 @@ class FileOverrider {
         }
 
         return selectedCartridge;
+    }
+
+    private sortCartridges(cartridges: string[]): Object[] {
+        const weightedCartridges = cartridges.map(cartridge => {
+            return {
+                name: cartridge,
+                weight: this.getWeightOfCartridge(cartridge)
+            };
+        });
+
+        weightedCartridges.sort((c1, c2) => {
+            return c2.weight - c1.weight;
+        });
+
+        return weightedCartridges;
+    }
+
+    private prefixWeight: Map<string, number> = new Map([
+        ['app', 10000],
+        ['int', 1000],
+        ['link', 1000],
+        ['plugin', 1000],
+        ['bc', 100]
+    ]);
+
+    private endOfList = ['modules', 'app_storefront_base'];
+
+    private getWeightOfCartridge(cartridgeName: string): number {
+        if (this.endOfList.includes(cartridgeName)) {
+            return -1000;
+        }
+
+        const prefix = /^([^_]*)_.*$/.exec(cartridgeName);
+
+        if (prefix) {
+            return this.prefixWeight.get(prefix[1]) || 0;
+        }
+
+        return 0;
     }
 
     protected getTargetPath(currentCartridge: SFCCCartridge) {
